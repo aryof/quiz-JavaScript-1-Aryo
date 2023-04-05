4d37c117cfa661a43fa2a546975d063bf1d464ba
aryof <128018962+aryof@users.noreply.github.com>
2023-04-05 16:59:05 +0700

    Add files via upload

---
+++ quiz js 1.js
@@ -0,0 +1,11 @@
+const evenNum = [0, 2, 4, 6, 8, 10, 12, 14, 16];
+
+for (let i = 0; i < evenNum.length; i++) {
+    console.log(evenNum[i]);
+}
+
+const a = a;
+
+    if (a % 2 === 0) {
+        console.log("bilangan genap")
+    }
